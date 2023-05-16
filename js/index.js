c1558df4d33526239cbe335eef4d7c404ff506f7
ViniciusCuest <viniciuscostargt@gmail.com>
2023-05-16 13:53:17 -0300

    Fixed nav funcionality and add some responsive rules

--- js/index.js
+++ js/index.js
@@ -1,9 +1,46 @@
+let activeIndex = 0;
+
+const html = document.getElementById("html");
+const icon = document.getElementById("icon-mode");
+
 const mode = document.getElementById("mode");
+const body = document.getElementById("body");
+const list = document.getElementById("list");
+
+const sections = body.clientHeight / 4;
+
+window.addEventListener("load", () => {
+   const mode = localStorage.getItem("mode");
+   if (mode === "dark") {
+      html.classList.add("dark");
+      icon.innerHTML = 'dark_mode';
+      return;
+   }
+   icon.innerHTML = 'light_mode';
+});
+
+body.onscroll = (evt) => {
+   const currentScroll = evt.currentTarget.scrollY;
+
+}
+
+function changeIndex(e) {
+   e.preventDefault();
+   const index = e.target.attributes.id.value;
+
+   if(activeIndex != index)
+      document.getElementById(activeIndex).removeAttribute('class');
+
+   activeIndex = index;
+   html.scroll({
+      top: sections * index,
+      behavior: 'smooth'
+   });
+}
 
 function iconAnimation(theme) {
 
    mode.removeChild(mode.firstElementChild);
-
 
    let spanIcon = document.createElement('span');
    spanIcon.innerHTML = theme;
@@ -16,11 +53,13 @@ function iconAnimation(theme) {
       localStorage.setItem("mode", "dark");
       return;
    }
-      html?.classList.remove("dark");
-      localStorage.clear();
+   html?.classList.remove("dark");
+   localStorage.clear();
 }
 
 mode.addEventListener("click", () => {
    const savedMode = localStorage.getItem("mode");
    savedMode ? iconAnimation('dark_mode') : iconAnimation('light_mode');
 });
+
+window.removeEventListener("load", () => { return; });
