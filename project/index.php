311815bf70143c694dc3f382bbc97fa1d90ca0ef
ViniciusCuest <viniciuscostargt@gmail.com>
2023-06-06 10:13:45 -0300

    changes

--- project/index.php
+++ project/index.php
@@ -11,7 +11,6 @@
 </head>
 
 <body class="project-body">
-<<<<<<< HEAD
     <div id="loading" style="display: none; padding: 0 5vw">
         <div style="
             display: grid;
@@ -20,32 +19,29 @@
             grid-column-gap: 10px;
             grid-row-gap: 10px;
             width: 400px;
-            height: 75vh
-        " id="db-img"></div>
-=======
-    <div id="loading" style="display: none">
-        <div id="db-img"></div>
->>>>>>> d4b3410b1c3b8771e27327ff2c9f8889e195c06e
-        <img id="load" style="animation: spinner 3s ease infinite; display: none" src="../src/assets/load-icn.png" alt="">
-        <div id="user-img"></div>
-    </div>
-    <main class="project-body-container">
-        <div class="prototype-mockup" id="prototype-mockup">
-            <img src="../src/assets/Background.png?V=<?php echo time() ?>" class="mockup">
-            <img src="../src/assets/splash-screen.png?V=<?php echo time() ?>" class="screenshot" />
+            height: 75vh" id="db-img"></div>
+        <div id="loading" style="display: none">
+            <div id="db-img"></div>
+            <img id="load" style="animation: spinner 3s ease infinite; display: none" src="../src/assets/load-icn.png" alt="">
+            <div id="user-img"></div>
         </div>
-        <form enctype="multipart/form-data">
-            <div>
-                <div id="divReference" style="opacity: .4" class="upload-single-image-container proj">
-                    <div id="uploadContainer" class="upload-single-image__upload-file-container">
-                        <input disabled id="fileInputReference" class="upload-single-image-file-input" type="file" name="img-diagnostico" accept="image/*">
-                    </div>
-                </div>
-                <button id="removeAll"> Limpar Imagens</button>
+        <main class="project-body-container">
+            <div class="prototype-mockup" id="prototype-mockup">
+                <img src="../src/assets/Background.png?V=<?php echo time() ?>" class="mockup">
+                <img src="../src/assets/splash-screen.png?V=<?php echo time() ?>" class="screenshot" />
             </div>
-            <button type="button" style="width: 50%; margin-top: 20px; height: 40px; border-radius: 8px; background-color: #fff; font-weight: 800; " id="submit" name="botao"> Enviar </button>
-        </form>
-    </main>
+            <form enctype="multipart/form-data">
+                <div>
+                    <div id="divReference" style="opacity: .4" class="upload-single-image-container proj">
+                        <div id="uploadContainer" class="upload-single-image__upload-file-container">
+                            <input disabled id="fileInputReference" class="upload-single-image-file-input" type="file" name="img-diagnostico" accept="image/*">
+                        </div>
+                    </div>
+                    <button id="removeAll"> Limpar Imagens</button>
+                </div>
+                <button type="button" style="width: 50%; margin-top: 20px; height: 40px; border-radius: 8px; background-color: #fff; font-weight: 800; " id="submit" name="botao"> Enviar </button>
+            </form>
+        </main>
 </body>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script type="text/javascript">
@@ -82,32 +78,20 @@
         });
         $("#fileInputReference").change((e) => {
             e.preventDefault();
-<<<<<<< HEAD
             if (e.currentTarget.files.length) {
                 const reader = new FileReader();
                 reader.readAsDataURL(e.currentTarget.files[0]);
                 reader.onload = () => {
                     $("#user-img").append(`<img style="width: 10vw" src="${reader.result}"/>`);
-=======
-            if(e.currentTarget.files.length) {
-                const reader = new FileReader();
-                reader.readAsDataURL(e.currentTarget.files[0]);
-                reader.onload = () => {
-                    $("#user-img").append(`<img width="15%" src="${reader.result}"/>`);
->>>>>>> d4b3410b1c3b8771e27327ff2c9f8889e195c06e
                 }
             }
         });
 
         $('#submit').click((e) => {
             e.preventDefault();
-<<<<<<< HEAD
-=======
-            
->>>>>>> d4b3410b1c3b8771e27327ff2c9f8889e195c06e
             if (!availableToSend)
                 return;
-                
+
             $.ajax({
                 type: "GET",
                 url: "../src/php/result.php",
@@ -125,7 +109,6 @@
                     console.log(err.responseText);
                 },
                 success: (response) => {
-<<<<<<< HEAD
                     response.data.map((item, index) => {
                         setTimeout(() => {
                             $("#db-img").append(`<img style="width: 100%" src="../src/img` + item.link_imagem.substring(6) + `"/>`);
@@ -134,35 +117,31 @@
                     setTimeout(() => {
                         $('.screenshot').remove();
                         $('.prototype-mockup').append('<img src="../src/assets/diagnostico.png" style="margin-top: 29px" class="screenshot" />');
-=======
-                    console.log(response);
-                    let imagem = (response.data.link_imagem.substring(6));
-                    console.log(imagem);
-                    $("#db-img").append(`<img width="15%" src="../src/img`+imagem+`"/>`);
-                    setTimeout(() => {
->>>>>>> d4b3410b1c3b8771e27327ff2c9f8889e195c06e
-                        $("#loading").removeClass('loading');
-                        $("#loading").css({
-                            display: "none"
-                        });
-                        $("#load").css({
-                            display: "none"
-                        });
-<<<<<<< HEAD
+                        console.log(response);
+                        let imagem = (response.data.link_imagem.substring(6));
+                        console.log(imagem);
+                        $("#db-img").append(`<img width="15%" src="../src/img` + imagem + `"/>`);
                         setTimeout(() => {
-                            $("#user-img").empty();
-                            $("#db-img").empty();
-                        }, 1000);
-                    }, 3000);
-=======
+                            $("#loading").removeClass('loading');
+                            $("#loading").css({
+                                display: "none"
+                            });
+                            $("#load").css({
+                                display: "none"
+                            });
+                            setTimeout(() => {
+                                $("#user-img").empty();
+                                $("#db-img").empty();
+                            }, 1000);
+                        }, 3000);
                         $('.prototype-mockup').append('<img src="../src/assets/diagnostico.png" id="camera" class="screenshot" />');
                     }, 2800);
 
->>>>>>> d4b3410b1c3b8771e27327ff2c9f8889e195c06e
                 }
             });
         });
     })
 </script>
 <script src="../src/js/DropDownFiles.js?v=<?php echo time(); ?>"></script>
+
 </html>
--- src/php/result.php
+++ src/php/result.php
@@ -11,21 +11,14 @@ $response = array(
 $query = $pdo->query("Select * from imagem_doenca");
 if ($query->execute()) {
     $result = $query->fetch();
-<<<<<<< HEAD
+
     $cod_disease = $result['cod_doenca'];
 
-=======
-    $cod_disease = $result['cod_doenca'][0];
->>>>>>> d4b3410b1c3b8771e27327ff2c9f8889e195c06e
     $data = $pdo->prepare("Select * from doenca d INNER JOIN imagem_doenca i ON i.cod_doenca = d.cod_doenca where d.cod_doenca = :cod ORDER BY d.cod_doenca DESC");
     $arr = array(':cod' => $cod_disease);
     if($data->execute($arr)) {
         $response['status'] = 200;
-<<<<<<< HEAD
         $response['data'] = $data->fetchAll();
-=======
-        $response['data'] = $data->fetch();
->>>>>>> d4b3410b1c3b8771e27327ff2c9f8889e195c06e
     }
 }
 
